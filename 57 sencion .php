a346c781177efc9c69a042499ee0a77f1514ee12
hafidznasution <rhafis709@gmail.com>
2024-10-07 09:37:39 +0700

    57 sencion

---
+++ 57 sencion .php
@@ -0,0 +1,15 @@
+<?php
+// Memulai sesi
+session_start();
+// Menyimpan data ke sesi
+$_SESSION["namaPengguna"] = "hafis code";
+// Mengakses data dari sesi
+echo 'Selamat Datang, ' . $_SESSION["namaPengguna"] . '!';
+
+// Menghapus variabel "namaPengguna" dari sesi
+unset($_SESSION["namaPengguna"]);
+// Menghancurkan seluruh sesi
+session_destroy();
+
+echo "<p><strong><i>by hafis ramadhan</strong>";
+?>
